0211dbbb7a0e919d4a8f2681e33599af71c7176d
Anthony Veyssiere <anthonyveyssiere@gmail.com>
2020-03-20 11:00:08 -0400

    fix config

--- public/API/config.php
+++ public/API/config.php
@@ -1,19 +1,17 @@
 <?php
 header('Access-Control-Allow-Origin: *');  
 
-
-//PARIS 21 NEEDS TO UPDATE THIS VALUES WITH THEIR SERVER CONFIG
+//PARIS 21 NEEDS TO UPDATE THIS PARAMETERES WITH THEIR SERVER CONFIG
 $selectedDB = 'paris21';
 $dbhost = 'localhost';
 $dbuser = 'root';
 $dbpass = 'root';
 
-
 $DBTABLES = array(
 	"indicators" => "indicators_mar2020",
 	"extractions" => "extractions",
 	"geobase" => "geobase_2019"
-)
+);
 
 $link = mysqli_connect($dbhost,$dbuser,$dbpass,$selectedDB) or die("Error " . mysqli_error($link));
 $link->set_charset("utf8");
